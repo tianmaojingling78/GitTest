b1908d6f7f7d1eece3de8e141de42581efe70cf0
zhaoduo <zhao13655035@163.com>
2024-09-08 22:30:04 +0800

    本地提交冲突

--- GitTest/src/main/java/com/example/gittest/HelloServlet.java
+++ GitTest/src/main/java/com/example/gittest/HelloServlet.java
@@ -11,6 +11,7 @@ public class HelloServlet extends HttpServlet {
 
     public void init() {
         message = "Hello World!";
+        System.out.println("本地修改");
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
